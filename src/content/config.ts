150275afe695cadbd31f58eb1b271e99d6645b56
John <batsikadzejoni@gmail.com>
2025-12-01 15:49:49 +0400

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,11 @@
-// src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 
-// "blog" — это название нашей коллекции
 const blogCollection = defineCollection({
-  // 'z' — это zod, он описывает "схему"
   schema: z.object({
-    title: z.string(), // У каждого поста должен быть заголовок (строка)
-    description: z.string(), // Описание для превью (строка)
-    pubDate: z.date(), // Дата публикации (дата)
-    // 'optional' значит "необязательно"
-    heroImage: z.string().optional(), // Картинка для обложки (строка - путь к файлу)
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    heroImage: z.string().optional(),
   }),
 });
 
